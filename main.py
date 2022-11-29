ba19279739306d74c954ad18c0d9b1d6a210deee
Dmitry Nazarov <demetrius.nazarov@yandex.ru>
2022-11-29 23:29:14 +0900

    Docstring fixed with pycodestyle (pep8).

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+# Тестовая игра с использованием функций
+
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Функция атаки."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный \
         {5 + randint(3, 5)}')
@@ -15,6 +19,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Функция защиты."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -25,6 +30,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Функция суперспособности."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение «Выносливость \
             {80 + 25}»')
@@ -36,6 +42,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Функция тренировки."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -60,6 +67,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Функция выбора класса."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -80,6 +88,8 @@ def choice_char_class() -> str:
 
 
 def main():
+    """Главная функция."""
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -90,4 +100,6 @@ def main():
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
 
-main()
+
+if __name__ == '__main__':
+    main()
